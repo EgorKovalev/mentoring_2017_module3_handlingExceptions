6f0d3e16499ecf4a87e85450e0b95b961e718f71
Yahor_Kavaliou <yahor.kavaliou@thomsonreuters.com>
2017-04-27 21:11:26 +0300

    Egor - add logger

--- ConsoleApplication1/Attributes/Logger/Logger.cs
+++ ConsoleApplication1/Attributes/Logger/Logger.cs
@@ -7,39 +7,7 @@ namespace Attributes.Logger
 {
     public class Logger
     {
-        private static readonly string Path = AppDomain.CurrentDomain.BaseDirectory;
-        private const string FatalFileName = @"\Fatal.txt";
-
-        public bool WriteExceptionToLog(string fileName, string ob, string message = "")
-        {
-            try
-            {
-                string fullPath = new StringBuilder(Path).Append(fileName).ToString();
-                string logMessage = string.Format("{0} - an exception has been threw in object with parameters: {1}", DateTime.Now.ToString(CultureInfo.InvariantCulture), ob);
-
-                if (!message.Equals(""))
-                {
-                    logMessage = message;
-                }
-                
-                WriteToLog(fullPath, logMessage);
-                return true;
-            }
-            catch (Exception)
-            {
-                string fullPath = new StringBuilder(Path).Append(FatalFileName).ToString();
-                string logMessage = string.Format("{0} - can't write to the log file: {1}", DateTime.Now.ToString(CultureInfo.InvariantCulture), fullPath);
-                WriteToLog(fullPath, logMessage);
-                return false;
-            }
-        }
-
-        public bool WriteExceptionToLog(string message)
-        {
-            return WriteExceptionToLog(FatalFileName, null, message);
-        }
-
-        private void WriteToLog(string path, string message)
+        public void Write(string path, string message)
         {
             using (StreamWriter sw = (File.Exists(path)) ? File.AppendText(path) : File.CreateText(path))
             {
--- ConsoleApplication1/Runner/Program.cs
+++ ConsoleApplication1/Runner/Program.cs
@@ -5,6 +5,8 @@ using Domain.Entities;
 using Domain.Models;
 using Services.Concrete;
 using System.Configuration;
+using System.Text;
+using Attributes.Logger;
 
 namespace Runner
 {
@@ -16,6 +18,8 @@ namespace Runner
 
         static void Main()
         {
+			string basePath = AppDomain.CurrentDomain.BaseDirectory;
+
             try
 			{
 				OrderService orderService = new OrderService();
@@ -29,15 +33,30 @@ namespace Runner
 			{				
 				string errorMessage = dlaEx.Message;
 				string additional = dlaEx.AdditionalDetails;
+
+				string path = new StringBuilder(basePath).Append(pathToDLALog).ToString();
+				string message = String.Format("{0} - data access layer exception has been threw in object {1}", errorMessage, additional);
+
+				new Logger().Write(path, message);
 			}
 			catch(BLBaseException blEx)
 			{
 				string errorMessage = blEx.Message;
 				string additional = blEx.AdditionalDetails;
+
+				string path = new StringBuilder(basePath).Append(pathToBLLog).ToString();
+				string message = String.Format("{0} - business layer exception has been threw in object {1}", errorMessage, additional);
+
+				new Logger().Write(path, message);
 			}
 			catch(SystemException ex)
 			{
 				string errorMessage = ex.Message;
+
+				string path = new StringBuilder(basePath).Append(pathToSysLog).ToString();
+				string message = String.Format("{0} - system exception has been threw", errorMessage);
+
+				new Logger().Write(path, message);
 			}
         }
     }
